a2480c38b204e65f852e7917093cf0d1ea1eab3b
carlos0716 <chulezyc@gmail.com>
2022-11-08 16:43:32 -0600

    Pass the sum function tests

--- calculator.js
+++ calculator.js
@@ -2,8 +2,14 @@ const add = (a, b) => a + b;
 	
 const subtract = (a , b) => a - b;
 
-const sum = function() {
-	
+const sum = function([...a]) { // the parameter for the function includes the rest parameter to allow to accept an indefinite number of arguments for the array
+	const array = [...a];
+  let initVal = 0;
+  const sumAll = array.reduce((previousValue, currentValue) => previousValue + currentValue, // here I use the reduce() method to sum all the elements of an array using an accumulator and a current value as parameters besides the callback function. 
+  initVal
+  );
+  return sumAll;
+  
 };
 
 const multiply = function() {
